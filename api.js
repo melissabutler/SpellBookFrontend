6c2e130ec84cf42d4fea0d534614b1c32726a948
melissabutler <melissaebutler2015@gmail.com>
2024-10-04 10:25:23 -0400

    troubleshooting vite env prod

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001"
+const BASE_URL = import.meta.env.BASE_URL || "http://localhost:3001";
 
 const DND_BASE_URL = 'https://www.dnd5eapi.co/api';
 /** API CLASS
